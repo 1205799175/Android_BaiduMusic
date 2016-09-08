7131089821142406364acf3c984850b52356a011
1205799175 <1205799175@qq.com>
2016-09-08 19:17:34 +0800

    build packages for BaiduMusic Project

--- BaiduMusic/app/src/main/java/com/yangyuning/baidumusic/controller/activity/MainActivity.java
+++ BaiduMusic/app/src/main/java/com/yangyuning/baidumusic/controller/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.yangyuning.baidumusic;
+package com.yangyuning.baidumusic.controller.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.yangyuning.baidumusic.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ BaiduMusic/app/src/main/java/com/yangyuning/baidumusic/controller/app/D.java
@@ -0,0 +1,7 @@
+package com.yangyuning.baidumusic.controller.app;
+
+/**
+ * Created by dllo on 16/9/8.
+ */
+public class D {
+}
